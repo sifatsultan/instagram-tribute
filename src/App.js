c20be422d0c54c24677ba7a12947d9f643b09bd5
Sifat Sultan <sifatsultan@gmail.com>
2022-05-11 16:37:44 +0600

    starting sign up section using modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Post from "./components/post/Post";
+import postsData from "./postsData";
+import { db } from "./firebase/FirebaseInit";
+import { Modal, Typography } from "@material-ui/core";
 
 
 function App() {
+  const [posts, setPosts] = useState(postsData)
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+
+  const [open, setOpen] = useState(false)
+  const handleSignup = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data()
+        }))
+      )
+    })
+  }, [])
+
   return (
     <div className="app">
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Typography>Text in a model</Typography>
+
+      </Modal>
       <div className="app__header">
         <div className="app__headerWrapper">
           <img
@@ -14,9 +57,21 @@ function App() {
           <button className="text__button">Logout</button>
           <div className="app__headerButtons">
             <button className="primary__button">Log in</button>
-            <button className="text__button">Sign up</button>
+            <button className="text__button" onClick={handleSignup} >Sign up</button>
           </div>
         </div>
+      </div>
+
+      <div className="timeline">
+
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            username={post.username}
+            caption={post.caption}
+            imageUrl={post.imageUrl}
+          />
+        ))}
       </div>
     </div>
   );
---
+++ src/postsData.js
@@ -0,0 +1,19 @@
+const postsData = [
+    {
+      id:1,
+      username: "blessingthebobo",
+      caption: "Subhan Allah!",
+      imageUrl:
+        "https://images.unsplash.com/photo-1487621167305-5d248087c724?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+    },
+    {
+      id:2,
+      username: "sifat",
+      caption: "The Majestic Allah",
+      imageUrl:
+        "https://images.unsplash.com/photo-1533323919903-fd18bacc10b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
+    },
+
+  ]
+
+  export default postsData
